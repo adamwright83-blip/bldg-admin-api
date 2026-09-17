48e5b340356cbdc16472d932fad844e51ea7a59c
Adam Wright <adam.wright83@gmail.com>
2026-09-16 19:09:59 -0700

    fix(driver): remove legacy menu override

--- client/src/pages/Driver.tsx
+++ client/src/pages/Driver.tsx
@@ -20,7 +20,6 @@ import "./goldline/goldline-driver-ui-forced-mobile.css";
 import "./goldline/goldline-driver-ui-contract.css";
 import "./goldline/goldline-driver-light-surfaces.css";
 import "./goldline/goldline-driver-cargo-light.css";
-import "./goldline/goldline-driver-menu-reachability.css";
 
 const WaywardTetheredDeck = lazy(
   () => import("./goldline/stages/WaywardTetheredDeck")
